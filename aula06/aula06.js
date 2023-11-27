59d7e56344a85da7251b1d436a892af2cc495a7a
jose50244966 <138632551+jose50244966@users.noreply.github.com>
2023-11-27 17:40:30 -0300

    Exercicio Aula 6

---
+++ aula06/aula06.js
@@ -0,0 +1,104 @@
+const input = require("readline-sync");
+
+const users = [
+    {
+        id: 1,
+        nome: "Pedro",
+        email: "pedro@gmail.com",
+    },
+    {
+        id: 2,
+        nome: "Tomaz",
+        email: "tomaz@gmail.com",
+    },
+    {
+        id: 3,
+        nome: "Fatima",
+        email: "fatima@hellcife.com",
+    },
+]
+let aplicativo = true
+do {
+    const cadastro = parseInt(input.question(`Bem vindo ao cadastro, qual acao deseja realizar:
+    1 - Novo cadastro
+    2 - Consultar cadastros
+    3 - Alterar cadastro
+    4 - Deletar cadastro
+    5 - Sair
+    `));
+    switch(cadastro){
+        case 1: { criarUser()} ;break;
+        case 2: { console.table(users); };break;
+        case 3: { attUser() };break;
+        case 4: { delUser() };break;
+        case 5: { aplicativo = false };break;
+        default: {
+            console.log("Erro !! escolha uma acao valida");
+        }
+    }
+
+}while(aplicativo)
+
+function criarUser(){
+    let id = parseInt(input.question("Digite o id do usuario: "));
+    let checkId = users.find((user) => user.id === id);
+    while(isNaN(id) || checkId){
+    id = parseInt(input.question("ID invalido, digite um novo ID: "));
+    checkId = users.find((user) => user.id === id);
+    } 
+    const user = {
+        id: id,
+        nome: input.question("Digite o nome do usuario: "),
+        email: input.question("Digite o e-mail do usuario: "),
+    }
+    console.log(user, "Foi adicionado com sucesso");
+    users.push(user);
+}
+
+function attUser(){
+    console.table(users)
+    let id = parseInt(input.question("Escolha o ID do usuario que deseja atualizar: "));
+    let user = users.find((usuario) => usuario.id === id);
+    if(user){
+        console.log(user, "Foi selecionado")
+    } else{
+        console.log("ERRO !!! Voce escolheu um ID invalido");
+        return
+    }
+    let invalid
+    do {
+        invalid = false
+        let userItem = parseInt(input.question(`Qual item do usuario deseja atualizar:
+        1 - Nome
+        2 - E-mail
+        `));
+        switch(userItem){
+            case 1: {
+                let novoNome = input.question("Digite o novo nome do usuario: ");
+                user.nome = novoNome;
+            }break;
+            case 2: {
+                let novoEmail = input.question("Digite o novo e-mail do usuario: ");
+                user.email = novoEmail;
+            }break;
+            default:{
+                console.log("ERRO !! Acao invalida");
+                invalid = true
+            }
+        }
+    } while(invalid)
+    console.log(user, "Foi atualizado com sucesso")
+}
+
+function delUser(){
+    console.table(users);
+    let id = parseInt(input.question("Escolha o ID do usuario que deseja excluir: "));
+    let user = users.findIndex((usuario) => usuario.id === id );
+    if(user === -1){
+        console.log("ERRO !! Voce digitou um ID invalido");
+        return
+    } else{
+        console.log(users[user], "Foi Deletado com sucesso")
+        users.splice(user, 1);
+    }
+}
