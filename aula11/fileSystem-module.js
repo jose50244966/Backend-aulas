2b18945601710b209016159f8a2dd8e081685399
jose50244966 <138632551+jose50244966@users.noreply.github.com>
2023-12-06 21:12:30 -0300

    Exercícios Aula 11

---
+++ aula11/fileSystem-module.js
@@ -0,0 +1,18 @@
+const fs = require("fs")
+
+//fs.readFile("path", "encode", function(err,data))
+
+/* fs.readFile("./arquivos/texto.txt", "utf-8", function(err,data){
+    if(err){
+        throw err
+    }
+    console.log(data)
+}) */
+
+//fs.appendFile("path", "data", function(err))
+
+//fs.writeFile("path", "data", function(err))
+
+//fs.unlink("path", function(err))
+
+//fs.rename("path", function(err))
