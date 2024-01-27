8affb2859a7422ec3bab6a12f20d6e5f0ad6599b
José Pedro <jose50244966@edu.pe.senac.br>
2024-01-26 23:50:42 -0300

    Ajustes no documento da aula 25

--- aula25/server.js
+++ aula25/server.js
@@ -1,14 +1,14 @@
 import http from 'http';
 
-const PORT = 3000
+const PORT = 3000;
 
 const rotas = {
     "/": "Projeto api clientes pagina principal",
     "/clientes" : "Pagina do clientees"
 }
 const server = http.createServer((req ,res) => {
-    res.writeHead(200, {"content-type": "text/plan"});
+    res.writeHead(200, {"content-type": "text/plain"});
     res.end(rotas[req.url]);
 })
 
-server.listen(PORT, ()=> console.log(`Escutando a porta ${PORT}`))
+server.listen(PORT, ()=> console.log(`Escutando a porta ${PORT}`));
