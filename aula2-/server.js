581301c3380e4e9494680ca058e1d706ccab9538
jose50244966 <138632551+jose50244966@users.noreply.github.com>
2024-01-26 21:23:16 -0300

    Aula 2? node modules

---
+++ aula2-/server.js
@@ -0,0 +1,14 @@
+import http from 'http';
+
+const PORT = 3000
+
+const rotas = {
+    "/": "Projeto api clientes pagina principal",
+    "/clientes" : "Pagina do clientees"
+}
+const server = http.createServer((req ,res) => {
+    res.writeHead(200, {"content-type": "text/plan"});
+    res.end(rotas[req.url]);
+})
+
+server.listen(PORT, ()=> console.log(`Escutando a porta ${PORT}`))
